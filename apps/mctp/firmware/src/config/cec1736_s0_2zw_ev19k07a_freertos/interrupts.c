7bc09ecb2b6ce707cc793489f8cdab7b912273f5
Shreyas Kannan <shreyas.kannan@microchip.com>
2023-02-16 19:27:23 +0530

    - DFP updated to v1.8.258 - Compiler updated to v4.21 SOTG3-1464

--- apps/mctp/firmware/src/config/cec1736_s0_2zw_ev19k07a_freertos/interrupts.c
+++ apps/mctp/firmware/src/config/cec1736_s0_2zw_ev19k07a_freertos/interrupts.c
@@ -79,9 +79,8 @@ void __attribute__((optimize("-O1"),section(".text.Dummy_Handler"),long_call, no
 }
 
 /* MISRAC 2012 deviation block start */
-/* MISRA C-2012 Rule 8.6 deviated 74 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
+/* MISRA C-2012 Rule 8.6 deviated 73 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
 /* Device vectors list dummy definition*/
-extern void MemMgnt_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
 extern void vPortSVCHandler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
 extern void xPortPendSVHandler         ( void ) __attribute__((weak, alias("Dummy_Handler")));
 extern void GIRQ08_Handler             ( void ) __attribute__((weak, alias("Dummy_Handler")));
@@ -195,7 +194,7 @@ const H3DeviceVectors exception_table=
     .pfnReset_Handler              = Reset_Handler,
     .pfnNonMaskableInt_Handler     = NonMaskableInt_Handler,
     .pfnHardFault_Handler          = HardFault_Handler,
-    .pfnMemMgnt_Handler            = MemMgnt_Handler,
+    .pfnMemoryManagement_Handler   = MemoryManagement_Handler,
     .pfnBusFault_Handler           = BusFault_Handler,
     .pfnUsageFault_Handler         = UsageFault_Handler,
     .pfnSVCall_Handler             = vPortSVCHandler,
